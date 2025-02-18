37743c6491bc2408ccaca1359440ff8761ccd536
Bill Zhou <80780626+zhoub1@users.noreply.github.com>
2025-02-18 14:43:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from lib.cdk_datazone_stack import CdkDatazoneStack
+
+# REPLACE ME WITH UNIQUE STACK NAME IF INVOKING MORE THAN ONCE
+unique_stack_name = f"CDKDataZoneMgmtStack"
+
+app = cdk.App()
+
+# Retrieve account and region from CDK context
+account = app.node.try_get_context('CDK_DEFAULT_ACCOUNT')
+region = app.node.try_get_context('CDK_DEFAULT_REGION')
+
+CdkDatazoneStack(
+    app,
+    unique_stack_name,
+    env=cdk.Environment(account=account, region=region)
+)
+
+app.synth()
