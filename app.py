d8ebc1fe28653c201d4f2e5c499cc9d029161be7
Bill Zhou <80780626+zhoub1@users.noreply.github.com>
2025-02-19 11:39:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import aws_cdk as cdk
 from lib.cdk_datazone_stack import CdkDatazoneStack
 
 # REPLACE ME WITH UNIQUE STACK NAME IF INVOKING MORE THAN ONCE
-unique_stack_name = f"pipelinedztestbz"
+unique_stack_name = f"pipelinedztestbz2"
 
 app = cdk.App()
 
