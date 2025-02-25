2d94e9d3b8ac470bc21c5dc9247270ae200e56fd
Bill Zhou <80780626+zhoub1@users.noreply.github.com>
2025-02-24 19:46:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import aws_cdk as cdk
 from lib.data_mesh_pipeline_stack import DataMeshPipelineStack
 
 # REPLACE ME WITH UNIQUE STACK NAME IF INVOKING MORE THAN ONCE
-unique_stack_name = f"CDKDataMeshPipelineStackJM"
+unique_stack_name = f"CDKDataMeshPipel022425"
 
 app = cdk.App()
 
